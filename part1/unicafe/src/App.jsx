b7dc58b6df9b67b0c4a455539db42f833cccee7a
Singh-Sagar <arcsagar.91@gmail.com>
2024-07-17 09:58:06 +0530

    unicafe step 2 completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -11,28 +11,82 @@ const Button = (props)=>{
 const Stats = (props)=>{
   
   return(<>
-  {/* console.log('started rendering stats') */}
-    <p><strong>{props.text} {props.value}</strong></p>
+    <p>{props.text} {props.value}</p>
   </>)
   
+}
+
+const Average = (props)=>{
+  if(props.total === 0){
+    return(<>
+      <p>average {0}</p>
+    </>)
+  }else{
+    return(
+      <>
+        <p>average {props.net/props.total}</p>
+      </>
+    )
+  }
 }
 
 const App = ()=>{
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [net, setNet] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const handleGood = ()=>{
+    setGood(good+1);
+    setNet(net + 1);
+    console.log(net);
+    setTotal(total+1);
+
+  }
+
+  const handleBad = ()=>{
+    setBad(bad+1);
+    setNet(net - 1);
+    console.log(net);
+    setTotal(total+1)
+  }
+
+  const handleNeutral = ()=>{
+    setNeutral(neutral+1);
+    setTotal(total+1);  
+
+  }
+  const Percentage = (props)=>{
+    if(props.total === 0){
+      return(
+        <div>
+          positive {0}
+        </div>
+      )
+    }else{
+      return(
+        <div>
+          positive {(good/total)*100}
+        </div>
+      )
+    }
+  }
 
   return(
     <div>
       <h1>give feedback</h1>
-      <Button handleClick={()=>setGood(good+1)} text={"good"}/>
-      <Button handleClick={()=>setNeutral(neutral+1)} text={"neutral"}/>
-      <Button handleClick={()=>setBad(bad+1)} text={"bad"}/>
+      <Button handleClick={handleGood} text={"good"}/>
+      <Button handleClick={handleNeutral} text={"neutral"}/>
+      <Button handleClick={handleBad} text={"bad"}/>
 
       <h1>statistics</h1>
       <Stats text={"good"} value={good}/>
       <Stats text={"neutral"} value={neutral}/>
       <Stats text={"bad"} value={bad}/>
+      <Stats text={"total"} value={good + bad + neutral}/>
+      <Average net={net} total={total}/>
+      <Percentage total={total} good={good}/>
 
     </div>
   )
